44b69a97879182bdbb278b6c3cf08194f31f113b
MuhammedAshrafSaleh <muhammedashrafsaleh@gmail.com>
2022-06-07 23:37:27 +0200

    Best Solution For Problem Fox And Number

--- #1 Ladder A/A. Fox and Number Game.cpp
+++ #1 Ladder A/A. Fox and Number Game.cpp
@@ -3,28 +3,15 @@
 using namespace std;
 int main() {
     int n; cin >> n;
-    int mn = 1;
     vector< int > v(n);
     for(int i =0; i < n; i++) {
         cin >> v[i];
-        mn = max(v[i],mn);
     }
+    int lastGCD = v[0];
     for(int i = 1; i < n ; i++) {
-        if(v[i] == v[i-1]) continue;
-        mn = min(mn,abs(v[i]-v[i-1]));
+        int gcd = __gcd(v[i],lastGCD);
+        lastGCD = gcd;
     }
-    while(true) {
-        bool AllSame = true;
-        sort(v.begin(),v.end());
-        for(int i = 0; i < n; i++) {
-            while(v[i] > mn) v[i] -= mn;
-            if(mn != v[i]) {
-              mn = min(mn, abs(mn - v[i]) );
-            }
-            if(i && v[i] != v[i-1]) AllSame = false;
-        }
-        if(AllSame || mn == 1) break;
-    }
-    cout<< 1LL * (mn * n);
+    cout<< lastGCD * n;
     return 0;
 }
