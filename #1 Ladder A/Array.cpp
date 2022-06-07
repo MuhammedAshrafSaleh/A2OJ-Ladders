cb93e177b7350dfc1e13d2540997123d1876c0b5
MuhammedAshrafSaleh <muhammedashrafsaleh@gmail.com>
2022-06-07 17:45:47 +0200

    Accepted

--- #1 Ladder A/Array.cpp
+++ #1 Ladder A/Array.cpp
@@ -2,6 +2,31 @@
 using namespace std;
 int main() {
     int n; cin >> n;
-    
+    deque < int > vp, vn, vz;
+    int p1= 1, p2= 1; 
+    for(int i =0; i < n; i++) {
+        int x; cin >> x;
+        if(x > 0 ) vp.push_back(x);
+        else if (x < 0) vn.push_back(x);
+        else vz.push_back(x);
+    }
+    if(vp.size() == 0) {
+        vp.push_back(vn.front());
+        vn.pop_front();
+        vp.push_back(vn.front());
+        vn.pop_front();
+    }
+    if((vn.size() & 1) == 0) {
+        vz.push_back(vn.front());
+        vn.pop_front();
+    }
+    cout << vn.size();
+    for(auto it : vn) cout <<" " << it;
+    cout << "\n";
+    cout << vp.size();
+    for(auto it : vp) cout <<" " << it;
+    cout << "\n";
+    cout << vz.size();
+    for(auto it : vz) cout <<" " << it;
     return 0;
 }
